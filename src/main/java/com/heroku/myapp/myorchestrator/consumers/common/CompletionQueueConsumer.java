96225443ab8dcb0d2d6fe241d74703416a91c1b0
doi-shuhei <doi@nagisalabs.org>
2016-08-24 18:33:45 +0900

    modify for Kind refactoring

--- src/main/java/com/heroku/myapp/myorchestrator/consumers/common/CompletionQueueConsumer.java
+++ src/main/java/com/heroku/myapp/myorchestrator/consumers/common/CompletionQueueConsumer.java
@@ -1,10 +1,10 @@
 package com.heroku.myapp.myorchestrator.consumers.common;
 
 import com.heroku.myapp.commons.config.enumerate.Kind;
+import com.heroku.myapp.commons.config.enumerate.KindOptions;
 import com.heroku.myapp.commons.consumers.QueueConsumer;
 import com.heroku.myapp.commons.util.actions.DiffUtil;
 import com.heroku.myapp.commons.util.actions.MasterUtil;
-import com.heroku.myapp.commons.util.consumers.QueueMessage;
 import static com.heroku.myapp.commons.util.consumers.QueueMessage.messageKindIs;
 import org.apache.camel.Exchange;
 import org.apache.camel.model.ChoiceDefinition;
@@ -26,16 +26,25 @@ public class CompletionQueueConsumer extends QueueConsumer {
                         -> new DiffUtil(exchange).enableDiff(this))
                 .choice();
         for (Kind k : Kind.values()) {
-            choice.when(messageKindIs(k)).to("log:" + k.expression());
+            if (k.optionIsEnable()) {
+                choice.when(messageKindIs(k)).to("log:" + k.expression());
+            }
         }
         choice.otherwise().to("log:none")
                 .end()
                 .choice()
                 .when(MasterUtil.isNotFilled(this))
                 .process(util().requestSnapshotProcess())
-                .otherwise()
-                .filter(QueueMessage.loadAffectPredicate())
+                .otherwise();
+
+        for(Kind k : Kind.values()){
+            if(k.optionIsEnable() && k.isEnable(KindOptions.affect)){
+                choice.when(messageKindIs(k))
+                        .multicast().to(k.affectQueueUriArray()).endChoice();
+            }
+        }
+        /*choice.filter(util().loadAffectPredicate())
                 .split().body()
-                .routingSlip(util().affectQueueUri());
+                .routingSlip(util().affectQueueUri());*/
     }
 }
