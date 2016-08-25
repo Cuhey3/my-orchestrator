9bec22fd055c45ba6958e3677894c13178cd4b4b
nanashi <nanashi@nanashi-pc>
2016-08-26 08:15:51 +0900

    modify ChangingQueueConsumer

--- src/main/java/com/heroku/myapp/myorchestrator/consumers/common/CompletionQueueConsumer.java
+++ src/main/java/com/heroku/myapp/myorchestrator/consumers/common/CompletionQueueConsumer.java
@@ -5,6 +5,7 @@ import com.heroku.myapp.commons.config.enumerate.KindOptions;
 import com.heroku.myapp.commons.consumers.QueueConsumer;
 import com.heroku.myapp.commons.util.actions.DiffUtil;
 import com.heroku.myapp.commons.util.actions.MasterUtil;
+import com.heroku.myapp.commons.util.consumers.QueueConsumerUtil;
 import static com.heroku.myapp.commons.util.consumers.QueueMessage.messageKindIs;
 import org.apache.camel.Exchange;
 import org.apache.camel.model.ChoiceDefinition;
@@ -24,27 +25,22 @@ public class CompletionQueueConsumer extends QueueConsumer {
                         -> new MasterUtil(exchange).snapshotSaveToMaster(this))
                 .filter((Exchange exchange)
                         -> new DiffUtil(exchange).enableDiff(this))
-                .choice();
-        for (Kind k : Kind.values()) {
-            if (k.optionIsEnable()) {
-                choice.when(messageKindIs(k)).to("log:" + k.expression());
-            }
-        }
-        choice.otherwise().to("log:none")
-                .end()
                 .choice()
                 .when(MasterUtil.isNotFilled(this))
                 .process(util().requestSnapshotProcess())
                 .otherwise();
 
-        for(Kind k : Kind.values()){
-            if(k.optionIsEnable() && k.isEnable(KindOptions.affect)){
-                choice.when(messageKindIs(k))
-                        .multicast().to(k.affectQueueUriArray()).endChoice();
+        for (Kind k : Kind.values()) {
+            if (k.optionIsEnable()) {
+                choice.when(messageKindIs(k)).to("log:" + k.expression());
+                if (k.isEnable(KindOptions.affect)) {
+                    k.affects().stream().forEach((affect) -> {
+                        choice.setBody().constant(affect.preMessage())
+                                .to(new QueueConsumerUtil(affect).snapshot()
+                                        .ironmqPostUri());
+                    });
+                }
             }
         }
-        /*choice.filter(util().loadAffectPredicate())
-                .split().body()
-                .routingSlip(util().affectQueueUri());*/
     }
 }
