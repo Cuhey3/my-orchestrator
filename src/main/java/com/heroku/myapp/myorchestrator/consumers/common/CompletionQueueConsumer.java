7d128b6569c6b2b8f107cccc050535ef250d09e0
doi-shuhei <doi@nagisalabs.org>
2016-09-02 17:21:05 +0900

    modify CompletionQueueConsumer to show route detail

--- src/main/java/com/heroku/myapp/myorchestrator/consumers/common/CompletionQueueConsumer.java
+++ src/main/java/com/heroku/myapp/myorchestrator/consumers/common/CompletionQueueConsumer.java
@@ -6,41 +6,75 @@ import com.heroku.myapp.commons.consumers.QueueConsumer;
 import com.heroku.myapp.commons.util.actions.DiffUtil;
 import com.heroku.myapp.commons.util.actions.MasterUtil;
 import com.heroku.myapp.commons.util.consumers.QueueConsumerUtil;
+import com.heroku.myapp.commons.util.consumers.QueueMessage;
 import static com.heroku.myapp.commons.util.consumers.QueueMessage.messageKindIs;
+import java.util.Optional;
 import org.apache.camel.Exchange;
 import org.apache.camel.model.ChoiceDefinition;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CompletionQueueConsumer extends QueueConsumer {
-
+    
     @Override
     public void configure() {
-        ChoiceDefinition choice = from(util().completion().ironmqConsumeUri())
-                .routeId(util().id())
-                .filter((Exchange exchange) -> {
+        ChoiceDefinition topLevelChoice = from(util().completion().ironmqConsumeUri())
+                .routeId(util().id()).choice();
+        setAlwaysAffects(topLevelChoice);
+        ChoiceDefinition secondLevelChoice
+                = topLevelChoice.otherwise().filter((Exchange exchange) -> {
                     return new MasterUtil(exchange).toCompleteLogic(this);
                 })
+                .id("filter:to_complete")
                 .filter((Exchange exchange)
                         -> new MasterUtil(exchange).snapshotSaveToMaster(this))
+                .id("filter:snapshot_save_to_master")
                 .filter((Exchange exchange)
                         -> new DiffUtil(exchange).enableDiff(this))
-                .choice()
-                .when(MasterUtil.isNotFilled(this))
+                .id("filter:enable_diff")
+                .choice();
+        ChoiceDefinition secondLevelChoiceOtherwise
+                = secondLevelChoice.when(MasterUtil.isNotFilled(this))
+                .id("when:isNotFilled")
                 .process(util().requestSnapshotProcess())
-                .otherwise();
-
+                .otherwise().choice();
+        
         for (Kind k : Kind.values()) {
             if (k.optionIsEnable()) {
-                choice.when(messageKindIs(k)).to("log:" + k.expression());
+                secondLevelChoiceOtherwise.when(messageKindIs(k)).to("log:" + k.expression());
                 if (k.isEnable(KindOptions.affect)) {
                     k.affects().stream().forEach((affect) -> {
-                        choice.setBody().constant(affect.preMessage())
+                        secondLevelChoiceOtherwise.setBody()
+                                .constant(affect.preMessage())
                                 .to(new QueueConsumerUtil(affect).snapshot()
                                         .ironmqPostUri());
                     });
                 }
             }
         }
+    }
+    
+    public void setAlwaysAffects(ChoiceDefinition choice) {
+        ChoiceDefinition nestedChoice = choice.when((Exchange exchange)
+                -> !Optional.ofNullable(
+                        (Boolean) new QueueMessage(exchange)
+                        .map().get("changed")).orElse(true))
+                .id("when:changedIsFalse")
+                .choice();
+        for (Kind k : Kind.values()) {
+            if (k.optionIsEnable()) {
+                if (k.isEnable(KindOptions.always_affect)) {
+                    nestedChoice.when(messageKindIs(k))
+                            .id("when:is_" + k.expression())
+                            .to("log:" + k.expression());
+                    k.alwaysAffects().stream().forEach((affect) -> {
+                        nestedChoice.setBody().constant(affect.preMessage())
+                                .to(new QueueConsumerUtil(affect).snapshot()
+                                        .ironmqPostUri());
+                    });
+                }
+            }
+        }
+        choice.endChoice();
     }
 }
