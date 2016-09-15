f55603621e272b685108bf3f9c9f4eba6d350c31
doi-shuhei <doi@nagisalabs.org>
2016-09-15 10:50:21 +0900

    modify DiffRecentchangesOfSeiyuConsumer logic

--- src/main/java/com/heroku/myapp/myorchestrator/consumers/snapshot/seiyu/DiffRecentchangesOfSeiyuConsumer.java
+++ src/main/java/com/heroku/myapp/myorchestrator/consumers/snapshot/seiyu/DiffRecentchangesOfSeiyuConsumer.java
@@ -17,19 +17,27 @@ public class DiffRecentchangesOfSeiyuConsumer extends DiffQueueConsumer {
     public Optional<Document> calculateDiff(Document master, Document snapshot) {
         List<Map<String, Object>> newList = new DocumentUtil().setDocument(snapshot).getData();
         List<Map<String, Object>> oldList = new DocumentUtil().setDocument(master).getData();
-        Map<Object, Map<String, Object>> oldMap = new LinkedHashMap<>();
-        oldList.stream().forEach((map) -> oldMap.put(map.get("title"), map));
+        Map<String, Map<String, Object>> oldMap = new LinkedHashMap<>();
+        oldList.stream().forEach((map) -> oldMap.put((String) map.get("title"), map));
         List<Map<String, Object>> collect = newList.stream()
                 .filter((map) -> map.containsKey("revid"))
                 .filter((map) -> {
-                    Object title = map.get("title");
+                    String title = (String) map.get("title");
                     if (oldMap.containsKey(title)) {
                         Map<String, Object> old = oldMap.get(title);
                         if (old.containsKey("revid")) {
-                            if (old.get("revid").equals(map.get("old_revid"))) {
+                            String old_new_revid = (String) old.get("revid");
+                            String new_old_revid = (String) map.get("old_revid");
+                            if (old_new_revid.equals(new_old_revid)) {
                                 return true;
                             } else {
-                                throw new RuntimeException();
+                                String old_old_revid = (String) old.get("old_revid");
+                                String new_new_revid = (String) map.get("revid");
+                                if (old_old_revid.equals(new_old_revid) && old_new_revid.equals(new_new_revid)) {
+                                    return false;
+                                } else {
+                                    throw new RuntimeException();
+                                }
                             }
                         } else {
                             return true;
