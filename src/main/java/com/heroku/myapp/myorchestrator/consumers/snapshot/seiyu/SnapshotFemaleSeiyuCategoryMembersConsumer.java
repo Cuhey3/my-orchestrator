262e8756285678c5f1faa728f5c884e0409184f0
doi-shuhei <doi@nagisalabs.org>
2016-09-29 17:57:01 +0900

    revert SnapshotFemaleSeiyuCategoryMembersConsumer

--- src/main/java/com/heroku/myapp/myorchestrator/consumers/snapshot/seiyu/SnapshotFemaleSeiyuCategoryMembersConsumer.java
+++ src/main/java/com/heroku/myapp/myorchestrator/consumers/snapshot/seiyu/SnapshotFemaleSeiyuCategoryMembersConsumer.java
@@ -1,21 +1,40 @@
 package com.heroku.myapp.myorchestrator.consumers.snapshot.seiyu;
 
+import com.heroku.myapp.commons.consumers.SnapshotQueueConsumer;
+import com.heroku.myapp.commons.util.content.DocumentUtil;
 import com.heroku.myapp.commons.util.content.MapList;
-import com.heroku.myapp.commons.consumers.SnapshotCategoryPagesAggregationConsumer;
+import com.heroku.myapp.commons.util.content.MediawikiApiRequest;
+import java.net.URLEncoder;
+import java.util.Optional;
+import org.apache.camel.Exchange;
+import org.bson.Document;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SnapshotFemaleSeiyuCategoryMembersConsumer extends SnapshotCategoryPagesAggregationConsumer {
-
-    public SnapshotFemaleSeiyuCategoryMembersConsumer() {
-        super();
-        addTargetCategory("Category:日本の女性声優");
-        cmpropParam("title|ids|sortkeyprefix");
-        includesCategoryFlag(false);
-    }
+public class SnapshotFemaleSeiyuCategoryMembersConsumer extends SnapshotQueueConsumer {
 
     @Override
-    protected void afterProcess(MapList mapList) {
-        mapList.forEach((m) -> m.put("gender", "f"));
+    protected Optional<Document> doSnapshot(Exchange exchange) {
+        try {
+            MapList result
+                    = new MediawikiApiRequest()
+                    .setApiParam("action=query&list=categorymembers"
+                            + "&cmtitle=Category:"
+                            + URLEncoder.encode("日本の女性声優", "UTF-8")
+                            + "&cmlimit=500"
+                            + "&cmnamespace=0"
+                            + "&format=xml"
+                            + "&continue="
+                            + "&cmprop=title|ids|sortkeyprefix")
+                    .setListName("categorymembers").setMapName("cm")
+                    .setContinueElementName("cmcontinue")
+                    .setIgnoreFields("ns")
+                    .getResultByMapList();
+            result.forEach((m) -> m.put("gender", "f"));
+            return new DocumentUtil(result).nullable();
+        } catch (Exception e) {
+            util().sendError("doSnapshot", e);
+            return Optional.empty();
+        }
     }
 }
